d1aef19793d39be826fc2900b4e331d49bbfc6fc
arix00 <15333224+arix00@users.noreply.github.com>
2024-11-24 23:07:25 -0800

    add build_sync

--- script/init_build.sh
+++ script/init_build.sh
@@ -2,11 +2,15 @@
 
 type="foss"  #foss nss
 ver="snapshot" #snapshot or release#
+sync="n" #sync with latest??
+tag="" #commit hash
 
 [ ! -z $1 ] && type=$1
 [ ! -z $2 ] && ver=$2
+[ ! -z $3 ] && sync=$3
+[ ! -z $4 ] && tag=$4
 
-if [ $type = "foss" ]; then
+if [ $type = "foss" ]; then    
     #use official PR from testuser7
     PATCH="https://github.com/openwrt/openwrt/pull/16070.diff"
 elif [ $type = "nss" -a $ver = "snapshot" ]; then
@@ -20,6 +24,22 @@ else
     exit 1
 fi    
 
+if [ "$ver" = "snapshot" ]; then
+  buildinfo="https://downloads.openwrt.org/snapshots/targets/qualcommax/ipq807x/version.buildinfo"
+else
+  buildinfo="https://downloads.openwrt.org/releases/$ver/targets/qualcommax/ipq807x/version.buildinfo"
+fi
+
+    
+if [ ! -z $tag ]; then
+    git checkout $tag
+    sync="n"
+fi    
+
+if [ $sync = "y" ]; then
+    #use published build version instead.
+    git checkout $(wget $buildinfo -O - | cut -d '-' -f 2)
+fi
 
 
 wget $PATCH -O mx4300.diff
