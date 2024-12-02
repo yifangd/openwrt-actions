4dad51675d34624b164eaa9107fdafd6fef74daf
arix00 <15333224+arix00@users.noreply.github.com>
2024-12-02 07:55:58 -0800

    fix 24.10 nss build

--- script/genconfig_nss.sh
+++ script/genconfig_nss.sh
@@ -18,8 +18,12 @@ fi
 if [ "${buildkmod}" != "n" ]; then
   jsonpath=$(wget ${mainpath}/sha256sums -O - | grep index.json -m 1 | cut -d '*' -f 2 | xargs)
   kmods=$(wget ${mainpath}/${jsonpath} -O - | jq '.packages' | grep \"kmod | cut -d '"' -f 2)
-  #kmods=$(echo "$kmods" | grep -v ath | grep -v kmod-bonding | grep -v vxlan | grep -v kmod-nat46)
-  kmods=$(echo "$kmods" | grep -v ath)
+  #fix dependency for 24.10 branch
+  if [ "$ver" != "${ver#24.10}" ]; then
+    kmods=$(echo "$kmods" | grep -v ath | grep -v kmod-bonding | grep -v vxlan | grep -v kmod-nat46)
+  else
+    kmods=$(echo "$kmods" | grep -v ath)
+  fi
   echo extra kmods: $(echo $kmods | wc -w)
 fi
 
@@ -32,6 +36,13 @@ CONFIG_FEED_nss_packages=n
 make defconfig
 
 for k in $kmods; do grep -q $k=y .config || echo CONFIG_PACKAGE_$k=m >> .config; done
+#nss patch has some dependency issue on 24.10 + kmods
+#to fix:
+#kmod-vxlan and kmod-nat46 must be compiled as y to keep kmod-qca-nss-ecm as y
+#must skip kmod-bonding or compile fail
+#no problem in snapshot as they all set to y
+[ "$ver" != "${ver#24.10}" ] && for i in vxlan nat46; do echo "CONFIG_PACKAGE_kmod-$i=y" >> .config; done
+
 make defconfig
 
 cat .config | grep kmod-qca | grep -v "not set"
